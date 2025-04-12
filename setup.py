ef2c4eb14c23183424638ec59849b58e1a461e95
Alejandro Flores <54118629+AleH3420@users.noreply.github.com>
2025-04-11 21:47:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='Paquete_mooc',
-  version= 1.0.0,
+  version= '1.0.0',
   packages= find_packages(),
   description= 'Paquete de prueba',
   author= 'A. Flz',
