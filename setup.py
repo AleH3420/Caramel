ef614c0ab5148c5d9f76bdd287e7adedf7ab09b8
Alejandro Flores <54118629+AleH3420@users.noreply.github.com>
2025-04-11 21:38:43 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='Paquete_mooc',
+  version= 1.0.0,
+  packages= find_packages(),
+  description= 'Paquete de prueba',
+  author= 'A. Flz',
+  author_email= 'alejandroflores3420@gmail.com',
+  url= 'https://github.com/AleH3420/Caramel',
+  install_requires= [
+    # Lista de bibliotecas necesarias
+  ],
+)
